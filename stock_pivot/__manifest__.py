e856a7a6700660ceef05761eababcf40bcd20e5d
votron <42276608+votron@users.noreply.github.com>
2018-10-30 09:31:03 +0530

    Update __manifest__.py

--- stock_pivot/__manifest__.py
+++ stock_pivot/__manifest__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name' : 'Modified Pivot View in ERP Application',
-    'version' : '1.6',
+    'version' : '1.7',
     'summary': 'Pivot View',
     'sequence': 30,
     'description': """
