191fdd1cde93c725c91a5d77b267707fadc95323
Deepak Nayak <deepak_nayak@seologistics.com>
2020-02-24 15:31:39 +0530

    Sale order confirmation alerts and remove customer from followers

---
+++ disable_followers/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Disable Automatic Followers on Sale Orders',
+    'summary': '',
+    'description': """
+Disable Automatic Followers on Sale Orders Confirm
+===============================
+    """,
+    'website': 'https://www.flexsin.com',
+    'depends': ['sale','mail'],
+    'category': 'Custom Dev',
+    'data': [
+
+    ],
+    'installable': True,
+    'auto_install': True,
+}
