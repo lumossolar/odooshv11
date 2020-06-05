cf6240936e8e4073b5c752cde485d3f8bbfee8ff
Deepak Nayak <deepak_nayak@seologistics.com>
2020-06-05 22:24:28 +0530

    Unique SKU Alert

--- unique_sku_alert/models/product.py
+++ unique_sku_alert/models/product.py
@@ -9,8 +9,8 @@ class ProductTemplate(models.Model):
     @api.constrains('default_code')
     def _check_default_code(self):
         code = self.search([('default_code', '=', self.default_code)])
-        print 'lengthhhhhhhhhhhhhhhh',len(code),self.default_code
-        if len(code) > 1:
+        # product = self.env['product.template'].search([('default_code', '=', self.default_code)])
+        if len(code) > 1 and not self.default_code == False:
             raise UserError(_('"SKU already exists!"'))
 
     # _sql_constraints = [
