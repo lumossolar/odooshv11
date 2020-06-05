0b2560457f6dc223d61aad3395b5d02d2f993577
Deepak Nayak <deepak_nayak@seologistics.com>
2020-06-05 21:38:09 +0530

    Unique SKU Alert

--- unique_sku_alert/models/product.py
+++ unique_sku_alert/models/product.py
@@ -1,11 +1,18 @@
-from odoo import api, fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
 
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
     _description = 'Product Template'
 
-    _sql_constraints = [
-        ('default_code_unique', 'unique (default_code)', 'SKU already exists!')]
+    @api.constrains('default_code')
+    def _check_default_code(self):
+        code = self.search([('default_code', '=', self.default_code)])
+        if len(code) > 1:
+            raise UserError(_('"SKU already exists!"'))
+
+    # _sql_constraints = [
+    #     ('default_code_unique', 'unique (default_code)', 'SKU already exists!')]
 
 
