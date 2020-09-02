db691ebc90e7ff8dad9de10b3216281b208e0812
Flexsin <testing.flexsin@gmail.com>
2020-09-02 13:31:58 +0530

    Update product.py

--- unique_sku_alert/models/product.py
+++ unique_sku_alert/models/product.py
@@ -6,14 +6,4 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
     _description = 'Product Template'
 
-    @api.constrains('default_code')
-    def _check_default_code(self):
-        code = self.search([('default_code', '=', self.default_code)])
-        # product = self.env['product.template'].search([('default_code', '=', self.default_code)])
-        if len(code) > 1 and not self.default_code == False:
-            raise UserError(_('"SKU already exists!"'))
-
-    # _sql_constraints = [
-    #     ('default_code_unique', 'unique (default_code)', 'SKU already exists!')]
-
-
+   
