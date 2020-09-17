2f68e6fa35bf833b162219023e220d5fd97c8805
Deepak Nayak <deepak_nayak@seologistics.com>
2020-09-17 14:01:47 +0530

    Disable Followers

---
+++ disable_followers/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Disable Automatic Followers on Sale Orders',
+    'summary': '',
+    'description': "Disable Automatic Followers on Sale Orders Confirm",
+    'website': 'https://www.flexsin.com',
+    'depends': ['mail'],
+    'category': 'Custom Dev',
+    'data': [
+
+    ],
+    'installable': True,
+    'auto_install': True,
+}
